7f8b57c1d2ac73594b1bfe41a4077729ff92862b
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2014-01-19 17:29:10 -0200

    Project restructured: - It was decided that the twee libraries, as they are, are enough for,   without modification, implement twee2sam; - All the twee-specific files were deleted, and twee was instead added as   a submodule; - Created a directory with the initial example files.

--- twee.py
+++ twee.py
@@ -2,7 +2,7 @@
 
 import sys, os, getopt, glob
 scriptPath = os.path.realpath(os.path.dirname(sys.argv[0]))
-sys.path.append(scriptPath + os.sep + 'lib')
+sys.path.append(os.sep.join([scriptPath, 'tw', 'lib']))
 from tiddlywiki import TiddlyWiki
 
 
@@ -13,12 +13,12 @@ def usage():
 def main (argv):
 
 	# defaults
-	
+
 	author = 'twee'
 	target = 'jonah'
 	merge = rss_output = ''
 	plugins = []
-	
+
 	# read command line switches
 
 	try:
@@ -26,7 +26,7 @@ def main (argv):
 	except getopt.GetoptError:
 		usage()
 		sys.exit(2)
-   	
+
 	for opt, arg in opts:
 		if (opt in ('-a', '--author')):
 			author = arg
@@ -38,67 +38,67 @@ def main (argv):
 			rss_output = arg
 		elif (opt in ('-t', '--target')):
 			target = arg
-	
+
 	# construct a TW object
-	
+
 	tw = TiddlyWiki(author)
-	
+
 	# read in a file to be merged
-	
+
 	if merge != '':
 		file = open(merge)
 		tw.addHtml(file.read())
 		file.close()
-	
-	# read source files		
-	
+
+	# read source files
+
 	sources = []
-	
+
 	for arg in args:
 		for file in glob.glob(arg):
 			sources.append(file)
-	
+
 	if len(sources) == 0:
 		print 'twee: no source files specified\n'
-		sys.exit(2)	
-	
+		sys.exit(2)
+
 	for source in sources:
-		file = open(source)		
+		file = open(source)
 		tw.addTwee(file.read())
 		file.close()
 
 	# generate RSS if requested
-	
+
 	if rss_output != '':
 		rss_file = open(rss_output, 'w')
 		tw.toRss().write_xml(rss_file)
 		rss_file.close()
-		
+
 	# output the target header
-	
-	if (target != 'none') and (target != 'plugin'):
-		file = open(scriptPath + os.sep + 'targets' + os.sep + target \
-								+ os.sep + 'header.html')
-		print(file.read())
-		file.close()
-	
+
+#	if (target != 'none') and (target != 'plugin'):
+#		file = open(scriptPath + os.sep + 'targets' + os.sep + target \
+#								+ os.sep + 'header.html')
+#		print(file.read())
+#		file.close()
+
 	# the tiddlers
-	
+
 	print tw.toHtml()
-	
+
 	# plugins
-	
+
 	for plugin in plugins:
 		file = open(scriptPath + os.sep + 'targets' + os.sep + target \
 								+ os.sep + 'plugins' + os.sep + plugin + os.sep + 'compiled.html')
 		print(file.read())
 		file.close()
-	
+
 	# and close it up
-	
+
 	print '</div></html>'
 
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
-	
+
