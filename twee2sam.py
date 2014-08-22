56d72c495ffe1185ff8f205a17f80262c15e9cea
Misty De Meo <mistydemeo@gmail.com>
2014-08-21 20:34:14 -0700

    twee2sam: replace open() with `with` blocks
    `with` blocks automatically close any opened files they receive at the
    end of the block, so this is a little safer than manually closing files
    as well as more concise.

--- twee2sam.py
+++ twee2sam.py
@@ -27,9 +27,8 @@ def main (argv):
 	# read in a file to be merged
 
 	if opts.merge:
-		file = open(opts.merge)
-		tw.addHtml(file.read())
-		file.close()
+		with open(opts.merge) as f:
+			tw.addHtml(f.read())
 
 	# read source files
 
@@ -40,9 +39,8 @@ def main (argv):
 		sys.exit(2)
 
 	for source in sources:
-		file = open(source)
-		tw.addTwee(file.read().decode('utf-8-sig'))
-		file.close()
+		with open(source) as f:
+			tw.addTwee(f.read().decode('utf-8-sig'))
 
 	src_dir = os.path.dirname(sources[0])
 
@@ -91,14 +89,11 @@ def main (argv):
 	def script_name(s):
 		return name_to_identifier(s) + '.twsam'
 
-	f_list = open(opts.destination + os.sep + 'Script.list.txt', 'w')
-
-	for passage_name in passage_order:
- 		passage = twp.passages[passage_name]
-		f_list.write(script_name(passage.title))
-		f_list.write('\n')
-
-	f_list.close()
+	with open(opts.destination + os.sep + 'Script.list.txt', 'w') as f_list:
+		for passage_name in passage_order:
+	 		passage = twp.passages[passage_name]
+			f_list.write(script_name(passage.title))
+			f_list.write('\n')
 
 
 	#
@@ -113,152 +108,146 @@ def main (argv):
 	image_list = []
 	music_list = []
 	for passage in twp.passages.values():
-		script = open(opts.destination + os.sep + script_name(passage.title), 'w')
+		with open(opts.destination + os.sep + script_name(passage.title), 'w') as script:
+			def check_print():
+				if check_print.pending:
+					script.write('!\n')
+					check_print.in_buffer = 0
+					check_print.pending = False
 
-		def check_print():
-			if check_print.pending:
-				script.write('!\n')
-				check_print.in_buffer = 0
-				check_print.pending = False
-
-		check_print.pending = False
-		check_print.in_buffer = 0
-
-		def warning(msg):
-			print 'Warning on {0}: {1}'.format(passage.title, msg)
-
-		def out_string(msg):
-			msg = msg.replace('"', "'").replace('[', '{').replace(']', '}');
-			msg_len = len(msg)
-
-			# Checks for buffer overflow
-			if check_print.in_buffer + msg_len > 511:
-				warning("The text exceeds the maximum buffer size; try to intersperse the text with some <<pause>> macros")
-				remaining = max(0, 511 - check_print.in_buffer)
-				msg = msg[:remaining]
-
-			script.write('"{0}"'.format(msg))
-			script.write('\n')
-
-			check_print.in_buffer += len(msg)
-
-		def out_expr(expr):
-			op, val = expr
-
-			if val is True:
-				script.write('1')
-			elif val is False:
-				script.write('0')
-			else:
-				script.write(variables.get_var(val))
-
-			if op == 'not':
-				script.write(' 0=')
-
-		# Outputs all the text
-
-		links = []
-
-		def register_link(cmd, is_conditional):
-			temp_var = variables.new_temp_var() if is_conditional else None
-			links.append((cmd, temp_var))
-			if temp_var:
-				script.write('1' + variables.set_var(temp_var))
-
-		def process_command_list(commands, is_conditional=False):
-			for cmd in commands:
-				if cmd.kind == 'text':
-					text = cmd.text.strip()
-					if text:
-						out_string(text)
-						check_print.pending = True
-				elif cmd.kind == 'image':
-					check_print()
-					if not cmd.path in image_list:
-						image_list.append(cmd.path)
-					script.write('{0}i\n'.format(image_list.index(cmd.path)))
-				elif cmd.kind == 'link':
-					register_link(cmd, is_conditional)
-					out_string(cmd.actual_label())
-				elif cmd.kind == 'list':
-					for lcmd in cmd.children:
-						if lcmd.kind == 'link':
-							register_link(lcmd, is_conditional)
-				elif cmd.kind == 'pause':
-					check_print.pending = True
-					check_print()
-				elif cmd.kind == 'set':
-					out_expr(cmd.expr)
-					script.write(variables.set_var(cmd.target) + '\n')
-				elif cmd.kind == 'if':
-					out_expr(cmd.expr)
-					script.write('[\n')
-					process_command_list(cmd.children, True)
-					script.write(' 0]\n')
-				elif cmd.kind == 'music':
-					if not cmd.path in music_list:
-						music_list.append(cmd.path)
-					script.write('{0}m\n'.format(music_list.index(cmd.path)))
-
-		process_command_list(passage.commands)
-
-		check_print()
-
-		# Builds the menu from the links
-
-		if links:
-			# Outputs the options separated by line breaks, max 28 chars per line
-			for link, temp_var in links:
-				if temp_var:
-					script.write('{0}['.format(variables.get_var(temp_var)))
-
-				out_string(link.actual_label()[:28] + '\n')
-
-				if temp_var:
-					script.write('0]\n')
-
-			script.write('?A.\n')
+			check_print.pending = False
 			check_print.in_buffer = 0
 
-			# Outputs the menu destinations
-			script.write('0B.\n');
+			def warning(msg):
+				print 'Warning on {0}: {1}'.format(passage.title, msg)
 
-			for link, temp_var in links:
+			def out_string(msg):
+				msg = msg.replace('"', "'").replace('[', '{').replace(']', '}');
+				msg_len = len(msg)
+
+				# Checks for buffer overflow
+				if check_print.in_buffer + msg_len > 511:
+					warning("The text exceeds the maximum buffer size; try to intersperse the text with some <<pause>> macros")
+					remaining = max(0, 511 - check_print.in_buffer)
+					msg = msg[:remaining]
+
+				script.write('"{0}"'.format(msg))
+				script.write('\n')
+
+				check_print.in_buffer += len(msg)
+
+			def out_expr(expr):
+				op, val = expr
+
+				if val is True:
+					script.write('1')
+				elif val is False:
+					script.write('0')
+				else:
+					script.write(variables.get_var(val))
+
+				if op == 'not':
+					script.write(' 0=')
+
+			# Outputs all the text
+
+			links = []
+
+			def register_link(cmd, is_conditional):
+				temp_var = variables.new_temp_var() if is_conditional else None
+				links.append((cmd, temp_var))
 				if temp_var:
-					script.write('{0}['.format(variables.get_var(temp_var)))
+					script.write('1' + variables.set_var(temp_var))
 
-				if not link.target in passage_indexes:
-					# TODO: Create a better exception
-					raise BaseException('Link points to a nonexisting passage: "{0}"'.format(link.target))
+			def process_command_list(commands, is_conditional=False):
+				for cmd in commands:
+					if cmd.kind == 'text':
+						text = cmd.text.strip()
+						if text:
+							out_string(text)
+							check_print.pending = True
+					elif cmd.kind == 'image':
+						check_print()
+						if not cmd.path in image_list:
+							image_list.append(cmd.path)
+						script.write('{0}i\n'.format(image_list.index(cmd.path)))
+					elif cmd.kind == 'link':
+						register_link(cmd, is_conditional)
+						out_string(cmd.actual_label())
+					elif cmd.kind == 'list':
+						for lcmd in cmd.children:
+							if lcmd.kind == 'link':
+								register_link(lcmd, is_conditional)
+					elif cmd.kind == 'pause':
+						check_print.pending = True
+						check_print()
+					elif cmd.kind == 'set':
+						out_expr(cmd.expr)
+						script.write(variables.set_var(cmd.target) + '\n')
+					elif cmd.kind == 'if':
+						out_expr(cmd.expr)
+						script.write('[\n')
+						process_command_list(cmd.children, True)
+						script.write(' 0]\n')
+					elif cmd.kind == 'music':
+						if not cmd.path in music_list:
+							music_list.append(cmd.path)
+						script.write('{0}m\n'.format(music_list.index(cmd.path)))
 
-				script.write('A:B:=[{0}j]'.format(passage_indexes[link.target]))
-				script.write('B:1+B.\n')
+			process_command_list(passage.commands)
 
-				if temp_var:
-					script.write('0]\n')
+			check_print()
 
-		else:
-			# No links? Generates an infinite loop.
-			script.write('1[1]\n')
+			# Builds the menu from the links
 
-		script.close()
+			if links:
+				# Outputs the options separated by line breaks, max 28 chars per line
+				for link, temp_var in links:
+					if temp_var:
+						script.write('{0}['.format(variables.get_var(temp_var)))
+
+					out_string(link.actual_label()[:28] + '\n')
+
+					if temp_var:
+						script.write('0]\n')
+
+				script.write('?A.\n')
+				check_print.in_buffer = 0
+
+				# Outputs the menu destinations
+				script.write('0B.\n');
+
+				for link, temp_var in links:
+					if temp_var:
+						script.write('{0}['.format(variables.get_var(temp_var)))
+
+					if not link.target in passage_indexes:
+						# TODO: Create a better exception
+						raise BaseException('Link points to a nonexisting passage: "{0}"'.format(link.target))
+
+					script.write('A:B:=[{0}j]'.format(passage_indexes[link.target]))
+					script.write('B:1+B.\n')
+
+					if temp_var:
+						script.write('0]\n')
+
+			else:
+				# No links? Generates an infinite loop.
+				script.write('1[1]\n')
 
 
 	#
 	# Function to copy the files on a list and generate a list file
 	#
 	def copy_and_build_list(list_file_name, file_list, item_extension, item_suffix = '', empty_item = 'blank'):
-		list_file = open(opts.destination + os.sep + list_file_name, 'w')
+		with open(opts.destination + os.sep + list_file_name, 'w') as list_file:
+			for file_path in file_list:
+				item_name = name_to_identifier(os.path.splitext(os.path.basename(file_path))[0])
+				list_file.write(item_name + item_suffix + '\n')
+				shutil.copyfile(src_dir + os.sep + file_path, opts.destination + os.sep + item_name + '.' + item_extension)
 
-		for file_path in file_list:
-			item_name = name_to_identifier(os.path.splitext(os.path.basename(file_path))[0])
-			list_file.write(item_name + item_suffix + '\n')
-			shutil.copyfile(src_dir + os.sep + file_path, opts.destination + os.sep + item_name + '.' + item_extension)
-
-		if not file_list:
-			list_file.write(empty_item + item_suffix + '\n')
-
-		list_file.close()
+			if not file_list:
+				list_file.write(empty_item + item_suffix + '\n')
 
 
 
